5f846f7e27ba5b63ab7cd8e80edae6d7020f8347
Xomcheg__Work <xromenkovevgenii@mail.ru>
2022-09-02 17:48:45 +0300

    исправлен баг запроса к серверу

--- src/components/actions/actions.js
+++ src/components/actions/actions.js
@@ -1,5 +1,4 @@
 export const btnAll = () => ({ type: 'btnAll' })
-// export const isAllCheckbox = () => ({ type: 'isAllCheckbox' })
 export const nonStop = () => ({ type: 'nonStop' })
 export const oneTransplants = () => ({ type: 'oneTransplants' })
 export const twoTransplants = () => ({ type: 'twoTransplants' })
@@ -10,31 +9,59 @@ export const loadingStatus = (status) => ({ type: 'loadingStatus', payload: stat
 export const getTicketsData = (arr) => ({ type: 'getTicketsData', payload: arr })
 export const getTickets = () => (dispatch) => {
   dispatch(loadingStatus(true))
-  // let id = ''
-  async function getTicket() {
+  const packegeTicketsArr = []
+  async function getPackegeId() {
     const id = await fetch('https://front-test.dev.aviasales.ru/search').then((res) => res.json())
-    // .then((body) => body)
-    // // .then((body) => fetch(`https://front-test.dev.aviasales.ru/tickets?searchId=${body.searchId}`))
-    // // .then((res) => res.json())
-    // // .then((body) => {
-    // //   console.log('body', body)
-    // //   const newTicketsArr = []
-    // //   for (let i = 0; i < 5; i += 1) {
-    // //     newTicketsArr.push(body.tickets[i])
-    // //   }
-    // //   dispatch(getTicketsData(newTicketsArr))
-    // //   dispatch(loadingStatus(false))
-    // // })
-    // .then((body) => body.searchId)
-    console.log('id', id.searchId)
-    let response
-    let result = []
-    do {
-      response = await fetch(`https://front-test.dev.aviasales.ru/tickets?searchId=${id.searchId}`).then((res) =>
+    console.log('firstId', id)
+    async function getPackegeTicket(ids) {
+      const response = await fetch(`https://front-test.dev.aviasales.ru/tickets?searchId=${ids}`).then((res) =>
         res.json()
       )
-      console.log('response', response)
-    } while (!response.stop)
+      console.log('firstResponse', response)
+      if (!response.stop) {
+        packegeTicketsArr.push(response)
+        getPackegeTicket(ids)
+      }
+      console.log('packegeTicketsArr', packegeTicketsArr)
+      return response
+    }
+    getPackegeTicket(id.searchId)
+    // const response = getPackegeTicket(id.searchId)
+    // packegeTicketsArr.push(response)
+    // if (!response.stop) {
+    //   getPackegeId()
+    // }
+    // console.log('secondRespose', response)
   }
-  getTicket()
+
+  getPackegeId()
+
+  //   // .then((body) => body)
+  //   // // .then((body) => fetch(`https://front-test.dev.aviasales.ru/tickets?searchId=${body.searchId}`))
+  //   // // .then((res) => res.json())
+  //   // // .then((body) => {
+  //   // //   console.log('body', body)
+  //   // //   const newTicketsArr = []
+  //   // //   for (let i = 0; i < 5; i += 1) {
+  //   // //     newTicketsArr.push(body.tickets[i])
+  //   // //   }
+  //   // //   dispatch(getTicketsData(newTicketsArr))
+  //   // //   dispatch(loadingStatus(false))
+  //   // // })
+  //   // .then((body) => body.searchId)
+  //   console.log('id', id.searchId)
+  //   let response
+  //   let result = []
+  //   do {
+  //     response = await fetch(`https://front-test.dev.aviasales.ru/tickets?searchId=${id.searchId}`).then((res) =>
+  //       res.json()
+  //     )
+  //     console.log('response', response)
+  //   } while (!response.stop)
+  // }
+  // async function getPackegeTickets() {
+  //   const response = await fetch(`https://front-test.dev.aviasales.ru/tickets?searchId=${body.searchId}`).then((res) => res.json())
+  //   return response
+  // }
+  // getPackegeId()
 }
